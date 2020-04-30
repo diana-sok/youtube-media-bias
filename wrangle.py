0003785cf297f55c05f377065d6d1a51d0c982ab
diana sok <dianadesok98@gmail.com>
2020-04-29 23:59:42 -0700

    wrangle original dataset

--- wrangle.py
+++ wrangle.py
@@ -1,9 +1,21 @@
+# Wrangles dataset.json
+# Wrangled to remove unnecessary data regarding channel objects
+
 import json
 
-# with open('wrangled-data.txt', 'w') as outfile:
-#     json.dump(data, outfile)
-
-with open('dataset.json') as json_file:
-    data = json.load(json_file)
-    # for i in range(2):
-    #     print(data[i])
+# opens a csv file for writing and the json file to wrangle
+with open('wrangled.json', 'a') as out_file, open('dataset.json', 'r') as \
+        in_file:
+    original_data = json.load(in_file)
+    for channel in original_data:
+        modified_channel = {"media": channel["media"],
+                            "youtube_id": channel["youtube_id"],
+                            "snippet": channel["snippet"],
+                            "statistics": channel["statistics"],
+                            "topicDetails": channel["topicDetails"],
+                            "videos_information": channel["videos_information"],
+                            "language_information": channel["language_information"],
+                            "bias": channel["bias"],
+                            }
+        json.dump(modified_channel, out_file)
+        out_file.write("\n")
